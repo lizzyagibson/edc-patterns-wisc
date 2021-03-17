d5d929a802dfa4045c98f2254d48485a8ebdc930
lizzyagibson <eag2186@cumc.columbia.edu>
2021-03-17 11:16:40 -0400

    Add Bayesian models to summary.

--- bn2mf_score_reg_int.stan
+++ bn2mf_score_reg_int.stan
@@ -2,26 +2,25 @@
 data {
   int<lower=0> N;   // number of observations
   int<lower=0> C;   // number of covariates
-  int<lower=0> K;   // number of patterns
   matrix[N, C] x;   // covariate matrix (minus sex)
   vector[N] y;      // outcome vector
   vector[N] sex;    // sex vector (separate for interaction)
   
-  matrix<lower=0>[N,K] ewa;    // mu for patterns
-  matrix<lower=0>[N,K] sd_ewa; // std dev for patterns
+  vector<lower=0>[N] ewa;    // mu for pattern
+  vector<lower=0>[N] sd_ewa; // std dev for pattern
 }
 
 // The parameters accepted by the model.
 parameters {
-  real alpha;              // intercept
+  real<lower=0> alpha;     // intercept
   real<lower=0> sigma;     // error scale
   
-  vector[C] beta_c;        // coefficients for covariates
-  real beta_int;      // coefficient for interaction
-  real beta_sex;      // coefficient for sex
+  vector[C] beta_c;    // coefficients for covariates
+  real beta_int;       // coefficient for interaction
+  real beta_sex;       // coefficient for sex
+  real beta_p;         // coefficients for patterns
   
-  vector[K] beta_p;         // coefficients for patterns
-  matrix<lower=0>[N,K] WA;  // pattern score with uncertainty
+  vector<lower=0>[N] WA;  // pattern scores with uncertainty
 }
 
 // interaction term here
@@ -30,10 +29,8 @@ parameters {
 // Are usually the arguments to the log-likelihood function that is evaluated in the model block, 
 // although in hierarchical models the line between the prior and the likelihood can be drawn in multiple ways
 transformed parameters {
-    vector<lower=0>[N] P1;
     vector<lower=0>[N] inter; // pattern score * sex
-    P1 = WA[,1]; // extract for interaction
-    inter = P1 .* sex;
+    inter = WA .* sex;
 }
 
 // The model to be estimated.
@@ -50,10 +47,9 @@ model {
   // beta_c ~ normal(0,5); // student_t(1, 0, 2.5);
   //  beta_p ~ normal(0,5); // student_t(1, 0, 2.5);
   
-  for (n in 1:N) { // prior on data
-    for (k in 1:K) {
-      WA[n,k] ~ normal(ewa[n,k], sd_ewa[n,k]);
-    }}
+  for (n in 1:N) { // draw data from this distribution
+      WA[n] ~ normal(ewa[n], sd_ewa[n]);
+    }
   
   y ~ normal(((WA * beta_p) + (inter * beta_int) + 
               (sex * beta_sex) + (x * beta_c) + alpha), sigma);  // likelihood
--- bn2mf_score_reg_p1.R
+++ bn2mf_score_reg_p1.R
@@ -17,7 +17,7 @@ var_wa <- readMat(here::here("./Data/mn2_WA_var_sd1.mat"))[[1]] %>% as_tibble() 
 bayes_int = bind_cols(ppp_cov, e_wa, var_wa) %>% 
         dplyr::select(-GEST, -ETH) %>% 
         rename(HOME_SCORE = HOMETOT) %>% 
-        filter(P2 < mean(P2) + 5*sd(P2)) %>% # removes two females, 1 male
+        filter(P2 < mean(P1) + 5*sd(P1)) %>% # removes two females, 1 male
         drop_na() %>% 
         mutate_at(vars(c(HOME_SCORE, M_AGE, M_IQ)), scale)
 bayes_int # 286 = n
@@ -29,80 +29,78 @@ x = model.matrix(WISC ~ M_AGE + M_EDU + MARITAL_STATUS + HOME_SCORE + M_IQ + ALC
                  data = bayes_int)[,-1]
 sex = model.matrix(WISC ~ SEX, data = bayes_int)[,-1]
 y = bayes_int$WISC
-P2 = bayes_int$P2
 P1 = bayes_int$P1
 
-sd_ewa = bayes_int %>% dplyr::select(varP2:varP1) %>% mutate_all(~sqrt(.))
+sd_ewa = bayes_int %>% dplyr::select(varP1:varP2) %>% mutate_all(~sqrt(.))
 sdP1 = sd_ewa$varP1
-sdP2 = sd_ewa$varP2
 
 # Run stan model ####
-p2_data = list(N = N,
+p1_data = list(N = N,
                   C = C,
                   x = x,
                   y = y,
                   sex = sex,
-                  ewa = P2,
-                  sd_ewa = sdP2)
+                  ewa = P1,
+                  sd_ewa = sdP1)
 
 tic()
-p2_fit <- stan(
+p1_fit <- stan(
   file = "bn2mf_score_reg_int.stan",  # Stan program
-    data = p2_data,               # named list of data
+    data = p1_data,               # named list of data
     chains = 4,                   # number of Markov chains
     warmup = 1000,                # number of warmup iterations per chain
     iter = 5000,                  # total number of iterations per chain
     cores = 2                     # number of cores
   )         
 toc()
-#save(p2_fit, file = "./Stan/p2_fit_nopriors.rda")
+#save(p1_fit, file = "./Stan/p1_fit_nopriors.rda")
 
-params = names(p2_fit)[grep("(alpha|sigma|beta)", names(p2_fit))]
+params = names(p1_fit)[grep("(alpha|sigma|beta)", names(p1_fit))]
 
 # Extract fit
-ext_p2 <- extract(p2_fit)
-str(ext_p2)
+ext_p1 <- extract(p1_fit)
+str(ext_p1)
 
 # predicted y values (draws x individuals)
-y_pred_p2 = ext_p2$y_tilde
+y_pred_p1 = ext_p1$y_tilde
 
 # estimated beta coefficients (draws x predictors)
-post_coef_p2 = cbind(ext_p2$alpha, ext_p2$sigma, ext_p2$beta_c, 
-                     ext_p2$beta_sex, ext_p2$beta_int, ext_p2$beta_p) %>% as_tibble()
-colnames(post_coef_p2) = params
-post_coef_p2
+post_coef_p1 = cbind(ext_p1$alpha, ext_p1$sigma, ext_p1$beta_c, 
+                     ext_p1$beta_sex, ext_p1$beta_int, ext_p1$beta_p) %>% as_tibble()
+colnames(post_coef_p1) = params
+post_coef_p1
 
 # random sample of y
 samp = sample(1:16000, 75) # (5000-1000 warmup) * 4
-y_post_samp_p2 = y_pred_p2[samp,]
-dim(y_post_samp_p2)
+y_post_samp_p1 = y_pred_p1[samp,]
+dim(y_post_samp_p1)
 
 # Summarize model ####
 
 # RMSE
-sqrt(mean((apply(y_pred_p2, 2, median) - y)^2))
+sqrt(mean((apply(y_pred_p1, 2, median) - y)^2))
 
 # n_eff is a crude measure of effective sample size, 
 # Rhat is the potential scale reduction factor on split chains (at convergence Rhat=1).
-print(p2_fit, c("lp__"))
-print(p2_fit, params)
+print(p1_fit, c("lp__"))
+print(p1_fit, params)
 
 # plot coefficients
-plot(p2_fit, pars = params, show_density=T, fill_color = "lightblue")
+plot(p1_fit, pars = params, show_density=T, fill_color = "lightblue")
 
 # Plot pattern beta coefficient with uncertainty
 color_scheme_set("brightblue")
-mcmc_areas(p2_fit,
+mcmc_areas(p1_fit,
            pars = params[(length(params)-2):length(params)],
            prob = 0.95) + ggtitle("Posterior distributions",
                                   "with medians and 95% intervals")
 
 # plot posterior prediction
 ppc_dens_overlay(y = as.vector(y),
-                 yrep = as.matrix(y_post_samp_p2))
+                 yrep = as.matrix(y_post_samp_p1))
 
 # posterior pred by grouping variable
-as.matrix(y_post_samp_p2) %>%
+as.matrix(y_post_samp_p1) %>%
   ppc_stat_grouped(y = as.vector(bayes_int$WISC),
                    group = as.vector(bayes_int$M_EDU),
                    stat = "median")
@@ -111,33 +109,33 @@ as.matrix(y_post_samp_p2) %>%
 color_scheme_set("brightblue")
 ppc_intervals(
   y = as.vector(y),
-  yrep = as.matrix(y_post_samp_p2),
-  x = as.vector(as.matrix(P2)),
+  yrep = as.matrix(y_post_samp_p1),
+  x = as.vector(as.matrix(P1)),
   prob = 0.5
 )
 
 # Plot regression line
-summary(p2_fit, params)$summary
+summary(p1_fit, params)$summary
 
-f_int = median(post_coef_p2$alpha)
-f_slope = median(post_coef_p2$`beta_p[1]`)
+f_int = median(post_coef_p1$alpha)
+f_slope = median(post_coef_p1$`beta_p[1]`)
 
 # Get credible interval
 samp = sample(1:16000, 500) # (5000-1000 warmup) * 4
-post_samp = post_coef_p2[samp, c(1, 9)]
+post_samp = post_coef_p1[samp, c(1, 9)]
 colnames(post_samp) = c("alpha", "beta")
 
-slope_25 = apply(post_coef_p2[,c(9)], 2, quantile, 0.025)
-slope_m = apply(post_coef_p2[,c(9)], 2, median)
-slope_75 = apply(post_coef_p2[,c(9)], 2, quantile, 0.975)
+slope_25 = apply(post_coef_p1[,c(9)], 2, quantile, 0.025)
+slope_m = apply(post_coef_p1[,c(9)], 2, median)
+slope_75 = apply(post_coef_p1[,c(9)], 2, quantile, 0.975)
 
-reg_pred = apply(y_pred_p2, 2, median)
-reg_low   = apply(y_pred_p2, 2, quantile, 0.025)
-reg_up   = apply(y_pred_p2, 2, quantile, 0.975)
-reg_sd   = apply(y_pred_p2, 2, sd)
+reg_pred = apply(y_pred_p1, 2, median)
+reg_low   = apply(y_pred_p1, 2, quantile, 0.025)
+reg_up   = apply(y_pred_p1, 2, quantile, 0.975)
+reg_sd   = apply(y_pred_p1, 2, sd)
 
 bayes_int %>% 
-  ggplot(aes(x = P2, ymin = reg_low, ymax = reg_up)) +
+  ggplot(aes(x = P1, ymin = reg_low, ymax = reg_up)) +
   geom_point(aes(y = WISC), color = "grey") +
   geom_smooth(aes(y = reg_pred, ymin = reg_low, ymax = reg_up), se=F,
               method = "lm") + 
@@ -149,19 +147,19 @@ bayes_int %>%
 
 # Check model fit ####
 
-# shinystan::launch_shinystan(p2_fit)
-traceplot(p2_fit, pars = params, inc_warmup = TRUE)
+# shinystan::launch_shinystan(p1_fit)
+traceplot(p1_fit, pars = params, inc_warmup = TRUE)
 
 # same as traceplots above
 # color_scheme_set("mix-blue-pink")
-# mcmc_trace(ext_p2, pars = params, n_warmup = 1000,
+# mcmc_trace(ext_p1, pars = params, n_warmup = 1000,
 #                 facet_args = list(nrow = 2, labeller = label_parsed))
 
 # check posterior
-lp_female = log_posterior(p2_fit)
+lp_female = log_posterior(p1_fit)
 #head(lp_female)
 #dim(lp_female)
-np_female <- nuts_params(p2_fit)
+np_female <- nuts_params(p1_fit)
 
 color_scheme_set("brightblue")
 mcmc_nuts_divergence(np_female, lp_female)
@@ -171,16 +169,16 @@ mcmc_nuts_divergence(np_female, lp_female)
 # this indicate that something is wrong with the model and the results should not be trusted
 color_scheme_set("darkgray")
 mcmc_scatter(
-  as.matrix(p2_fit),
+  as.matrix(p1_fit),
   pars = params[7:8],
-  np = nuts_params(p2_fit),
+  np = nuts_params(p1_fit),
   np_style = scatter_style_np(div_color = "red", div_alpha = 0.8)
 )
 
 # error
 color_scheme_set("brightblue")
 ppc_error_hist(y = as.vector(y),
-               yrep = as.matrix(y_post_samp_p2)[1:6,])
+               yrep = as.matrix(y_post_samp_p1)[1:6,])
 
 # log posterior
 ggplot(lp_female, aes(x = Iteration, y = Value, color = as.factor(Chain))) +
@@ -195,12 +193,12 @@ ggplot(lp_female, aes(x = Iteration, y = Value, color = as.factor(Chain))) +
 # if all chains are at equilibrium, these will be the same and 𝑅̂  will be one. 
 # If the chains have not converged to a common distribution, the 𝑅̂  statistic 
 # will be greater than one 
-rhats <- rhat(p2_fit)
+rhats <- rhat(p1_fit)
 mcmc_rhat(rhats)
 
 # The larger the ratio of 𝑛𝑒𝑓𝑓 to 𝑁 the better
 # a useful heuristic is to worry about any 𝑛𝑒𝑓𝑓/𝑁 less than 0.1.
-ratios_cp <- neff_ratio(p2_fit)
+ratios_cp <- neff_ratio(p1_fit)
 #names(ratios_cp)[1:11]
 mcmc_neff(ratios_cp[names(ratios_cp) %in% params], size = 2)
 
@@ -209,5 +207,5 @@ mcmc_neff(ratios_cp[names(ratios_cp) %in% params], size = 2)
 # Positive autocorrelation is bad (it means the chain tends to stay in the same area between 
 # iterations) and you want it to drop quickly to zero with increasing lag. Negative autocorrelation 
 # is possible and it is useful as it indicates fast convergence of sample mean towards true mean.
-mcmc_acf_bar(p2_fit, pars = params, lags = 10)
+mcmc_acf_bar(p1_fit, pars = params, lags = 10)
 
